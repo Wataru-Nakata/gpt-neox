ebc491459dc0052fa155b3dc7f0d63aeca30f6d0
Wataru Nakata <wataru9871@gmail.com>
2023-10-11 18:43:32 +0900

    update script

--- scripts/gslm/1node.sh
+++ scripts/gslm/1node.sh
@@ -3,11 +3,12 @@
 #$ -l h_rt=72:00:00
 #$ -j y
 #$-cwd
+source /etc/profile.d/modules.sh
 module load python/3.10/3.10.10
 module load cuda/12.1/12.1.1
 module load cudnn/8.9/8.9.2
 module load nccl/2.18/2.18.1-1
 source venv/bin/activate
 pip install -r requirements/requirements-wandb.txt
-pip install -r requirements.lock
+pip install -r requirements/requirements.txt
 python ./deepy.py train.py configs/1-3B.yml scripts/gslm/dataset.yml
